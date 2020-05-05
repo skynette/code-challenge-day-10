0cd4ee2dda2823947e190a7a5403436773d7317c
L0n3w4lf <cutejosh2@gmail.com>
2020-05-05 19:20:08 +0100

    solved

--- pangram.py
+++ pangram.py
@@ -1,2 +1,12 @@
 def is_pangram(sentence):
-    pass
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    sentence = sentence.replace(" ", "")
+    sentence = sentence.replace(".", "")
+    sentence = sentence.replace('\"', '')
+    if len(sentence) < 26:
+        return False
+    sentence = set(sentence.lower())
+    alphabets = set(letters)
+    if len(sentence&alphabets) != 26:
+        return False
+    return True
