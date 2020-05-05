4e2f110a7451c25d1997d024de4c8ed09a5a2126
skynette <cutejosh2@gmail.com>
2020-05-05 19:23:38 +0100

    Revert "solved"

--- pangram.py
+++ pangram.py
@@ -1,12 +1,2 @@
 def is_pangram(sentence):
-    letters = "abcdefghijklmnopqrstuvwxyz"
-    sentence = sentence.replace(" ", "")
-    sentence = sentence.replace(".", "")
-    sentence = sentence.replace('\"', '')
-    if len(sentence) < 26:
-        return False
-    sentence = set(sentence.lower())
-    alphabets = set(letters)
-    if len(sentence&alphabets) != 26:
-        return False
-    return True
+    pass
